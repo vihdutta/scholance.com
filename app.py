4ed4d319257aa158c031f169582a1b79052f6229
vihdutta <vihdutta@gmail.com>
2023-04-22 20:37:31 -0400

    created MVP pre-launch page

--- app.py
+++ app.py
@@ -1,336 +1,86 @@
-from flask import (Flask, jsonify, render_template, request,
-                   redirect, session, flash, url_for)
-from datetime import datetime
-from hashlib import sha256
+from flask import (Flask, render_template, request, flash,
+                   redirect, url_for)
 from pymongo import MongoClient
-from bson import ObjectId, json_util
-import json
+import sib_api_v3_sdk
 import certifi
 
-cluster = MongoClient("mongodb+srv://civichours:zTudxFA2GtQN8xP7@cluster0.ovv1ops.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+# Mongo DB Setup
+cluster = MongoClient("mongodb+srv://scholance:3kxzhyifLA5kVeDk@cluster0.ovv1ops.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = cluster["main"]
 users_db = db["users"]
 projects_db = db["projects"]
 
+# SendInBlue Setup
+sib_configuration = sib_api_v3_sdk.Configuration()
+sib_configuration.api_key['api-key'] = "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"
+sib_api_transactional_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(sib_configuration))
+sib_api_contacts_instance = sib_api_v3_sdk.ContactsApi(sib_api_v3_sdk.ApiClient(sib_configuration))
+
+# Flask Setup
 app = Flask(__name__)
 app.secret_key = "SECRET"
 
+# General Definitions
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
 
 @app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == "POST" :
-        search_query = request.form.get('search')
-        if search_query:
-            return redirect(url_for("search", search_query=search_query))
-        else:
-            return redirect('/')
-        
+def index():        
+    if request.method == "POST":
+        send_contact_email()
     return render_template("/index.html")
 
-@app.route("/about")
+@app.route('/organizations', methods=["GET", "POST"])
+def organizations():
+    if request.method == "POST":
+        send_contact_email()
+    return render_template("/organizations.html")
+
+
+@app.route('/about', methods=["GET", "POST"])
 def about():
     return render_template("/about.html")
 
-@app.route("/organizations")
-def organizations():
-    return render_template("/organizations.html")
+def send_contact_email():
+    question_email = request.form.get('question-email')
+    try:
+        first_name = request.form.get('name').split(" ")[0]
+        last_name = request.form.get('name').split(" ")[1]
+    except Exception:
+        first_name = request.form.get('name')
+        last_name = ""
+    message = request.form.get('message')
 
-@app.route('/login', methods=["POST", "GET"])
-def login():
-    if session.get("username"):
-        return redirect(url_for("dashboard"))
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+    create_contact = sib_api_v3_sdk.CreateContact(email=question_email, update_enabled=True, attributes={'FIRSTNAME': first_name, 'LASTNAME': last_name})
 
-        if username == "" or password == "":
-            flash("Username or password field empty", "error")
-            return redirect(request.url)
-        user_in_database = users_db.find_one({"username":username})
+    template_id = 2
+    sender = {"name": "Scholance", "email": "info@scholance.com"}
+    to = [{"email": question_email, "name": first_name + last_name}]
+    params = {'FIRSTNAME': first_name, 'LASTNAME': last_name, "EMAIL": question_email}
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, template_id=template_id, sender=sender, headers=headers, params=params)
 
-        if not user_in_database:
-            flash("Invalid username or password!", "error")
-            return redirect(request.url)
-        
-        user_password = user_in_database["password"]
-            
-        if sha256(password.encode('utf-8')).hexdigest() == user_password:
-            session["username"] = user_in_database["username"]
-            return redirect(url_for("dashboard"))
+    sib_api_contacts_instance.create_contact(create_contact)
+    sib_api_transactional_instance.send_transac_email(send_smtp_email)
+    flash("Thank you for submitting! We'll respond shortly.", "info")
+    return redirect(request.url)
 
-        flash("Invalid username or password!", "error")
-        return redirect(url_for("login"))
-    return render_template("/login.html")
+@app.route('/api/request-early-access/<email>', methods=["GET"])
+def request_early_access(email):
+    create_contact = sib_api_v3_sdk.CreateContact(email=email, update_enabled=True)
 
+    template_id = 3
+    sender = {"name": "Scholance", "email": "info@scholance.com"}
+    to = [{"email": email, "name": "New Scholancer"}]
+    params = {"EMAIL": email}
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, template_id=template_id, sender=sender, headers=headers, params=params)
 
-@app.route("/projects_check")
-def projects_check():
-    projects_data = list(projects_db.find())
-    return f"Projects: {projects_data}"
-
-
-@app.route('/dashboard')
-def dashboard():
-    if not session.get("username"):
-        flash("You are not logged in!", "error")
-        return redirect(url_for("login"))
-    else:
-        owned_projects, joined_projects, applied_projects = user_project_data()
-
-        return render_template("/dashboard/dashboard.html",
-                               owned_projects=owned_projects,
-                               joined_projects=joined_projects,
-                               applied_projects=applied_projects)
-
-
-@app.route("/projects/post", methods=["POST", "GET"])
-def postjob():
-    if not session.get("username"):
-        flash("You are not logged in!", "error")
-        return redirect(url_for("login"))
-    else:
-        if request.method == "POST":
-            description = request.form.get("description")
-            goals = request.form.get("goals")
-            time_commitment = request.form.get("time_commitment")
-            expectations = request.form.get("expectations")
-            tags = request.form.get("tags")
-            if tags != None:
-                tags = request.form.get("tags").split(",")
-                tags = [tag.strip() for tag in tags]
-            project_id = ObjectId()
-
-            project = {"_id":project_id,
-                    "name": request.form.get("project_name"),
-                    "owner": session.get("username"),
-                    "volunteers": [],
-                    "applications": [],
-                    "size": request.form.get("size"),
-                    "start":datetime.now().strftime("%m%d%Y"),
-                    "goals": goals,
-                    "time_commitment": time_commitment,
-                    "expectations": expectations,
-                    "description": description,
-                    "tags": tags,
-                    "active": True}
-            
-            user_in_database = users_db.find_one({"username":session.get("username")})
-            user_projects_len = len(user_in_database.get("owned_projects"))
-
-            if user_projects_len > 4:
-                flash("You already have 5 projects!", "error")
-                return redirect(request.url)
-            else:
-                projects_db.insert_one(project)
-                users_db.update_one({"username":session.get("username")}, {"$push": {"owned_projects": project_id}})
-
-            flash("Project posted successfully!", "info")
-            return redirect(request.url)
-        else:
-            return render_template("/projects/post-project.html")
-
-@app.route('/logout')
-def logout():
-    session.pop("username")
-    return redirect(url_for("index"))
-
-
-@app.route('/signup', methods=["POST", "GET"])
-def signup():
-    if session.get("username"):
-        return redirect(url_for("dashboard"))
-    if request.method == "POST":
-        username = request.form.get("username")
-        email = request.form.get("email")
-        password = request.form.get("password")
-
-        if username == "" or email == "" or password == "":
-            flash("Fill out all fields!", "error")
-            return redirect(url_for("signup"))
-
-        user_in_database = users_db.find_one({"username":username})
-        if user_in_database:
-            flash("Username already exists!", "error")
-            return redirect(url_for("signup"))
-
-        full_name = request.form.get("full_name")
-
-        if len(full_name.split(" ")) == 2:
-            first_name = full_name.split(" ")[0]
-            last_name = full_name.split(" ")[1]
-        else:
-            first_name = full_name
-            last_name = ""
-
-        new_user = {"_id": ObjectId(),
-            "admin": False,
-            "username": request.form.get("username"),
-            "first_name":first_name,
-            "last_name":last_name,
-            "email": request.form.get("email"),
-            "zip": None,
-            "password":sha256(request.form.get("password").encode('utf-8')).hexdigest(),
-            "phone_number": None,
-            "owned_projects": [],
-            "joined_projects": [],
-            "applied_projects": []}
-
-        users_db.insert_one(new_user)
-
-        flash("Account successfully created!", "info")
-        return redirect(url_for("login"))
-    return render_template("/signup.html")
-
-
-@app.route('/profile/user')
-def user_profile():
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    username = request.form['username']
-    email = request.form['email']
-    school = request.form['school']
-
-    # Check if any fields are empty
-    update_dict = {}
-    if first_name:
-        update_dict['first_name'] = first_name
-    if last_name:
-        update_dict['last_name'] = last_name
-    if username:
-        update_dict['username'] = username
-    if email:
-        update_dict['email'] = email
-    if school:
-        update_dict['school'] = school
-
-    users_db.update_one({'username': session["username"]}, {'$set': update_dict})
-    return render_template("/profile/user-profile.html")
-
-
-@app.route('/profile/business')
-def business_profile():
-    return render_template("/profile/business-profile.html")
-
-
-@app.route('/profile/password')
-def password():
-    return render_template("/profile/password.html")
-
-
-@app.route("/projects")
-def projects():
-    data = list(projects_db.find())
-    return render_template("/projects/projects.html", data=json.loads(json_util.dumps(data)), data_length=len(data))
-
-
-@app.route("/projects/apply/<project_id>", methods=["GET"])
-def apply(project_id):
-
-    project = projects_db.find_one({"_id": ObjectId(project_id)})
-    project_owner = project.get("owner")
-    project_volunteers = project.get("volunteers")
-    project_applications = project.get("applications")
-    print(session["username"])
-    print(project_owner)
-    print(project_volunteers)
-    print(project_applications)
-    if session["username"] != project_owner and session["username"] not in project_volunteers and session["username"] not in project_applications:
-            users_db.update_one({"username": session["username"]}, {'$push': {"applied_projects": ObjectId(project_id)}})
-            projects_db.update_one({"_id": ObjectId(project_id)}, {'$push': {"applications": session["username"]}})
-            print(f"{session['username']} + {project_owner} + {project_volunteers} + {project_applications}")
-    else:
-        flash(f"{session['username']} + {project_owner} + {project_volunteers} + {project_applications}", "error")
-        return redirect(url_for("projects"))
-    return redirect(url_for("dashboard"))
-
-@app.route("/api/project-category-request/<project_category>", methods=["GET"])
-def project_data_request(project_category):
-    owned_projects, joined_projects, applied_projects = user_project_data()
-    if project_category == "owned-projects":
-        return json.loads(json_util.dumps(owned_projects))
-    if project_category == "joined-projects":
-        return json.loads(json_util.dumps(joined_projects))
-    if project_category == "applied-projects":
-        return json.loads(json_util.dumps(applied_projects))
-    return "ERROR 404: NO DATA FOUND"
-
-@app.route("/api/project-delete/<project_id>", methods=["GET"])
-def project_delete(project_id):
-
-    projects_db.find_one_and_delete({"_id":ObjectId(project_id)})
-
-    users_db.update_many(
-        {
-            "$or": [
-                {"owned_projects": ObjectId(project_id)},
-                {"joined_projects": ObjectId(project_id)},
-                {"applied_projects": ObjectId(project_id)}
-            ]
-        },
-        {
-            "$pull": {
-                "owned_projects": ObjectId(project_id),
-                "joined_projects": ObjectId(project_id),
-                "applied_projects": ObjectId(project_id)
-            }
-        }
-    )
-    return jsonify("COMPLETE")
-
-def user_project_data():
-    user_data = users_db.find_one({"username":session["username"]})
-    owned_project_ids = user_data["owned_projects"]
-    joined_project_ids = user_data["joined_projects"]
-    applied_projects_ids = user_data["applied_projects"]
-
-    owned_projects = []
-    joined_projects = []
-    applied_projects = []
-
-    for _id in owned_project_ids:
-        owned_project = projects_db.find_one({"_id": _id})
-        if owned_project:
-            owned_projects.append(owned_project)
-    for _id in joined_project_ids:
-        joined_project = projects_db.find_one({"_id": _id})
-        if joined_project:
-            joined_projects.append(joined_project)
-    for _id in applied_projects_ids:
-        applied_project = projects_db.find_one({"_id": _id})
-        if applied_project:
-            applied_projects.append(applied_project)
-
-    return owned_projects, joined_projects, applied_projects
-
-# @app.route("/projects/<project_id>")
-# def project_page(project_id):
-#     if project_id.isnumeric() == False:
-#         flash("Invalid link!", "error")
-#         return redirect(url_for("projects"))
-
-#     data = list(projects_db.find())
-#     for project_data in data:
-#         if str(project_data["_id"]).zfill(7) == project_id:
-#             return render_template("/projects/project-page.html", project_data=project_data)
-#     return "<h1>Project Not Found!</h1>"
-
-
-# @app.route("/projects/<project>/apply", methods=["POST", "GET"])
-# def apply_for_project(project):
-#     projects = dynamo.scan_projects("project_url")
-#     print("Projects:")
-#     for i, p in enumerate(projects):
-#         p_url = projects[i]["info"]["project_url"]
-#         if p_url == project:
-#             pn = projects[i]["project_name"]
-#             p = dynamo.get_project(pn)
-#             print(p)
-#             pa = p["info"]['applications']
-#             pa.append(user.username)
-#             dynamo.update_project(pn, "applications", pa)
-#         else:
-#             print("p_url != project")
-#     return "<h1>Applied</h1>"
+    sib_api_contacts_instance.create_contact(create_contact)
+    sib_api_transactional_instance.send_transac_email(send_smtp_email)
+    return "REQUEST SUCCESSFUL"
 
 
 
---
+++ static/javascript/script.js
@@ -0,0 +1,78 @@
+const quotes = [
+  "'I can't believe how easily I can help others from my laptop.' - T.G.",
+  "'Online community service is the future. Proud to join it!' - C.H.",
+  "'I never thought I could make such a big difference online.' - A.M.",
+  "'Online volunteering is perfect for students like me.' - E.L.",
+  "'There's so much to gain from this, and nothing to lose.' - B.N.",
+  "'My community service hours will be much easier to earn.' - W.E.",
+  "'Doing the school's requirements from home is awesome.' - H.J.",
+  "'Just wow. Do you know how much I need this right now?' - G.S.",
+  "'This is the most helpful thing I've found in a while.' - J.W.",
+  "'I'm gonna need this. I'm so glad I can use it now.' - Y.L.",
+  "'It's so simple to sign up and use. Really incredible.' - G.J.",
+  "'This is a lifesaver. No joke, it's so helpful to me.' - M.P."
+];
+
+// Initialize the quote index to 0
+var quoteIndex = 0;
+
+// Function to change the quote
+function changeQuote() {
+  // Fade out the current quote
+  $("#quotes").fadeOut(500, function () {
+    // Get the current quote from the array
+    var currentQuote = quotes[quoteIndex];
+    // Update the text of the quote element
+    $(this).text(currentQuote);
+    // Increment the quote index
+    quoteIndex++;
+    // If we've reached the end of the array, start over
+    if (quoteIndex >= quotes.length) {
+      quoteIndex = 0;
+    }
+    // Fade in the new quote
+    $(this).fadeIn(500);
+  });
+}
+
+// Call the function to change the quote initially after the document has loaded
+$(document).ready(function () {
+  changeQuote();
+  // Change the quote automatically every 2.5 seconds
+  setInterval(changeQuote, 3000);
+});
+
+$(document).ready(function () {
+  // When the button with id "scroll" is clicked
+  $('#scroll').click(function () {
+    // Animate scrolling to the top of the page
+    var inputBox = $("#main-email");
+    inputBox.focus();
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
+
+    // Return false to prevent default behavior of the button
+    return false;
+  });
+});
+
+// Function to make the AJAX request for requesting early access
+function requestEarlyAccess(email) {
+  $.ajax({
+    type: "GET",
+    url: "/api/request-early-access/" + email,
+    success: function(data) {
+      console.log("Request early access request successful!");
+      console.log("Status: " + data.status);
+    },
+    error: function() {
+      console.log("Request early access request failed.");
+    }
+  });
+}
+// Trigger the request early access request when the request early access form is submitted
+$("#request-early-access").on("submit", function(e) {
+  e.preventDefault();
+  var email = $(this).find("#main-email").val();
+  $(this).find("#main-email").val("Thank you! We'll send access through your email soon!");
+  requestEarlyAccess(email);
+});
